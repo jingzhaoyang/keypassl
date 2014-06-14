104fe8d05b954c89703e7c5557180e59cebf55f4
jingzhaoyang <jingzhaoyang@163.com>
2014-06-14 20:35:38 +0800

    删除keypassl的无用代码

--- keypassl_1.py
+++ keypassl_1.py
@@ -2,54 +2,21 @@
 #coding=utf-8
 
 import sys,threading
-from PyQt4 import QtGui, QtCore, uic
-# from PyQt4.QtCore import  QStringList,QCoreApplication
-# from evdev import InputDevice, categorize, ecodes
 from select import select
 import virtkey,time
 from kptool.keepassdb import keepassdb
 
-# keyboard_dev = InputDevice('/dev/input/event5')
-# mouse_dev = InputDevice('/dev/input/event6')
-password = raw_input('请输入密码：',)
+# password = raw_input('请输入密码：',)
+password = ""
 
-hot_key_down = 0
-ctrl_key_value = 0
 def put_key(key):
     v = virtkey.virtkey()
     for i in str(key):
         v.press_unicode(ord(i))
         v.release_unicode(ord(i))
-    #v.press_keysym(65363)
-    #v.release_keysym(65363)
-# def hot_ctrl_key():
-    
-#     global ctrl_key_value
-#     while True:
-#         r,w,x = select([keyboard_dev],[],[])
-#         for event in keyboard_dev.read():
-#             if event.type == ecodes.EV_KEY and event.code == 29:
-#                 ctrl_key_value = event.value
 class MyWindow( QtGui.QWidget ):
     def __init__( self ):
         super( MyWindow, self ).__init__()
-        # self.setWindowTitle( "hello" )
-        # self.resize( 300, 200 )
-        
-        # t = threading.Thread(target=hot_ctrl_key,args=())
-        # t.start()
-        # t1 = threading.Thread(target=self.HotKey)
-        # t1.start()
-        # self.monitor_action()
-        # gridlayout = QtGui.QGridLayout()
-        # self.button = QtGui.QPushButton( u"打开密码框" )
-        # gridlayout.addWidget( self.button )
-        # self.setLayout( gridlayout )
-         
-        # self.connect( self.button, QtCore.SIGNAL( 'clicked()' ), self.creat_menu)
-        # self.timer = QtCore.QTimer()
-        # self.connect(self.timer,QtCore.SIGNAL("timeout()"), self.monitor_action)
-        # self.timer.start(100)
         self.creat_menu()
         self.doExit()            
     def monitor_action(self):
@@ -91,7 +58,6 @@ class MyWindow( QtGui.QWidget ):
         point = QtGui.QCursor.pos()
         self.menu[0].exec_(point)
         self.menu[0].close()
-        # self.doExit()
 
 
     def get_userandpass(self,k,groupid):
@@ -117,16 +83,8 @@ class MyWindow( QtGui.QWidget ):
         self.close()
         sys.exit(0)
         
-    # def HotKey(self):
-    #     global hot_key_down
-    #     while True:
-    #         r,w,x = select([mouse_dev],[],[])
-    #         for event in mouse_dev.read():
-    #             if ctrl_key_value != 0:
-    #                 if event.type == ecodes.EV_KEY and event.value == 1 and event.code == 272:
-    #                     hot_key_down = 1
-
-app = QtGui.QApplication( sys.argv )
-demo = MyWindow()
-demo.show()
-app.exec_()
+if __name__ == '__main__':
+    app = QtGui.QApplication( sys.argv )
+    demo = MyWindow()
+    demo.show()
+    app.exec_()
